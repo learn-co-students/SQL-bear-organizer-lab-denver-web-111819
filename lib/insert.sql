8ee7f8adc91d9f719d2759fdfeb46e66ee0ee4b0
meniality <patrh_99@hotmail.com>
2019-11-26 14:20:07 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES ("Mr. Chocoalte",21,"male","brown","hostile",true);
+
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES ("Rowdy",21,"male","brown","hostile",true);
+
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES ("Tabitha",21,"male","brown","hostile",true);
+
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES ("Segeant Brown",21,"male","brown","hostile",true);
+
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES ("Melissa",21,"male","brown","hostile",true);
+
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES ("Grinch",21,"male","brown","hostile",true);
+
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES ("Wendy",21,"male","brown","hostile",true);
+
+INSERT INTO bears (name, age, gender,color,temperament,alive)
+VALUES (NULL,21,"male","brown","hostile",true);
